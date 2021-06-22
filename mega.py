4e6c7b0f22d63f21331bdbcd55ea2f61ccd5e4d3
justaprudev <justaprudev@gmail.com>
2021-06-22 13:07:16 +0530

    mega: just some tests

--- mega.py
+++ mega.py
@@ -3,76 +3,77 @@ import asyncio
 from os import kill
 from pathlib import Path
 from signal import SIGTERM
+from client import TelegramClient
+from telethon.events import StopPropagation
 from pymegatools import Megatools, MegaError
 
+client: TelegramClient
 megatools = Megatools()
-dldir = Path("mega")
+dest = Path("mega")
 
-
-@polygon.on(pattern="mega -c ?(.*)")
-async def megakill(e):
+@client.interface(pattern="mega -c ?(.*)")
+async def mega_cancel(event):
     try:
-        pid = int(e.pattern_match.group(1))
+        pid = int(
+            event.pattern_match.group(1)
+        )
         kill(pid, SIGTERM)
     except (TypeError, ValueError, ProcessLookupError):
-        await e.edit("`Download not found. (Invalid PID)`")
+        await event.edit("`Download not found. (Invalid PID)`")
     else:
-        await e.edit(f"`Successfully killed download with pid {pid}`")
+        await event.edit(f"`Successfully killed download with pid {pid}`")
+    raise StopPropagation
 
+@client.interface(pattern="mega ?(.*)")
+async def mega(event):
+    if not dest.exists():
+        dest.mkdir()
 
-@polygon.on(pattern="mega ?(.*)")
-async def mega(e):
-    if not dldir.exists():
-        dldir.mkdir()
-
-    url = e.pattern_match.group(1)
+    url = event.pattern_match.group(1)
     filename = None
 
     if not url:
         # No url provided
-        return await e.edit((
+        return await event.edit((
             f"Megatools v`{megatools.version}`"
             "\n`.mega url`: Download files from mega.nz to mega folder"
             "\n`.mega -c pid`: Cancel an active download with its pid"
         ))
-    elif url.startswith("-c"):
-        # Call to cancel handler
-        return None
 
-    await e.edit("`Trying to download..`")
+    await event.edit("`Trying to download..`")
 
     try:
         filename = megatools.filename(url)
         _, returncode = await megatools.download(
             url,
             progress=telegram_progress,
-            progress_arguments=(e,),
-            path=dldir.name
+            progress_arguments=(event,),
+            path=dest.name
         )
     except MegaError as err:
         returncode = err.returncode
 
-    outputs = {
-        -1: f"Whoops! Something went wrong, Return code: {returncode}",
+    unknown_error = f"Whoops! Something went wrong, Return code: {returncode}"
+    code_to_string = {
         0: f"Downloaded {filename} successfully." if filename else "Invalid URL.",
         1: "File already exists!\nCheck the mega folder.",
         -15: f'Download for "{filename}" was cancelled'
     }
 
     try:
-        await e.edit(f"`{outputs.get(returncode) or outputs.get(-1)}`")
+        await event.edit(f"`{code_to_string.get(returncode, unknown_error)}`")
     except Exception:
         pass
 
 
-async def telegram_progress(stream, process, e):
+async def telegram_progress(stream, process, event):
     progress = stream[-1]
     if progress:
         match = re.match(
             "(.*): (.*) - (.*) \(.*\) of (.*) \((.*)\)", progress
         )
         if match:
-            await e.edit(
+            await event.edit(
                 f"**Downloading:** `{match.group(1)}`\
             \n**Progress:** `{match.group(2)}`\
             \n**Downloaded:** `{match.group(3)}`\
